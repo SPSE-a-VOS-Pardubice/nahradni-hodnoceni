3e13e48066dd47145f5ff4476d5b0508e72bf005
pvorisek25 <pvorisek25@protonmail.com>
2023-01-18 16:22:03 +0100

    Frontend importu dat
    Co-Authored-By: Radim Semerák <108821590+radimsem@users.noreply.github.com>
    Co-Authored-By: Tobiáš Smolný <86459914+TobiasSmolny@users.noreply.github.com>

--- view/import.php
+++ view/import.php
@@ -7,27 +7,29 @@ use Spse\NahradniHodnoceni\Model\ViewableProperty;
 use Spse\NahradniHodnoceni\Model\ViewablePropertyType;
 
 ?>
+
 <!DOCTYPE html>
 <html lang="cs">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Import dat</title>
-    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="/assets/css/global.css">
-    <link rel="stylesheet" href="/assets/css/table.css">
+    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="/assets/css/global.css" />
+    <link rel="stylesheet" href="/assets/css/import.css" />
 </head>
 
 <body>
     <?php include(VIEW_ROOT . "/component/header.php") ?>
     <main>
-        <p>Importing files</p>
         <form action="" method="post">
-            <button name="foo" value="upvote">Upload</button>
+            <button type="submit">Import</button>
+            <input type="file" id="import" name="import" accept=".csv">
         </form>
     </main>
+
 </body>
 
 </html>
--- view/importPreview.php
+++ view/importPreview.php
@@ -11,20 +11,66 @@ use Spse\NahradniHodnoceni\Model\ViewablePropertyType;
 <html lang="cs">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Náhled importu</title>
-    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="/assets/css/global.css">
-    <link rel="stylesheet" href="/assets/css/table.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Import dat - table</title>
+    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="/assets/css/global.css" />
+    <link rel="stylesheet" href="/assets/css/import.css" />
 </head>
 
 <body>
     <?php include(VIEW_ROOT . "/component/header.php") ?>
+
     <main>
-        <p>Import Preview</p>
+        <section class="table_sec">
+
+
+            <table>
+                <thead>
+                    <tr class="tr-inputs">
+                        <th>Jméno</th>
+                        <th>Příjmení</th>
+                        <th>Známka</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <a href="#">
+                                <div style="width: 100%;height: 100%;">Vojtěch</div>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="#">
+                                <div style="width: 100%;height: 100%;">Fošnár</div>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="#">
+                                <div style="width: 100%;height: 100%;">5</div>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">Libor</a></td>
+                        <td><a href="#">Bajer</a></td>
+                        <td><a href="#">1</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">Petr</a></td>
+                        <td><a href="#">Fišar</a></td>
+                        <td><a href="#">3</a></td>
+                    </tr>
+                </tbody>
+            </table>
+            <form action="" method="post">
+                <button type="submit">Akceptovat</button>
+            </form>
+        </section>
     </main>
+
 </body>
 
 </html>
