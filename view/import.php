c284044f3fc3a955b95237076b2f073fb2c59ff8
pvorisek25 <pvorisek25@protonmail.com>
2023-01-18 15:13:40 +0100

    Přidání provizorních souborů CSV

---
+++ view/import.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Spse\NahradniHodnoceni\Model\DatabaseEntity;
+use Spse\NahradniHodnoceni\Model\ViewableProperty;
+use Spse\NahradniHodnoceni\Model\ViewablePropertyType;
+
+define("itemsIntermediateData", $args["data"]["itemsIntermediateData"]);
+define("options",               $args["data"]["options"]);
+
+echo ($args["data"]["test"]);
+
+?>
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>General Table</title>
+  <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="/assets/css/global.css">
+  <link rel="stylesheet" href="/assets/css/table.css">
+
+</head>
+
+<body>
+  <?php include(VIEW_ROOT . "/component/header.php") ?>
+  <main>
+    <p>Importing files</p>      
+  </main>
+</body>
+
+</html>
