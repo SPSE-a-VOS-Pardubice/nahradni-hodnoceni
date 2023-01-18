43a6db7b5ec786f508c8a93de3ad96db490314c8
Vojtěch Fošnár <vfosnar@fosny.eu>
2023-01-18 12:14:49 +0100

    uprava dat pro testovani mezitabulek

--- docs/data.sql
+++ docs/data.sql
@@ -1,11 +1,13 @@
 -- příznaky
 INSERT INTO `Traits` (`name`) VALUES ('Visual Studio');
 INSERT INTO `Traits` (`name`) VALUES ('Adobe Software');
+INSERT INTO `Traits` (`name`) VALUES ('Počítače');
+INSERT INTO `Traits` (`name`) VALUES ('Tělocvična');
 -- učebny
-INSERT INTO `Classrooms` (`label`) VALUES ('D253');
-INSERT INTO `Classrooms` (`label`) VALUES ('D353');
-INSERT INTO `Classrooms` (`label`) VALUES ('C317');
-INSERT INTO `Classrooms` (`label`) VALUES ('A103');
+INSERT INTO `Classrooms` (`label`) VALUES ('A000');
+INSERT INTO `Classrooms` (`label`) VALUES ('B069');
+INSERT INTO `Classrooms` (`label`) VALUES ('C128');
+INSERT INTO `Classrooms` (`label`) VALUES ('D420');
 -- předměty
 INSERT INTO `Subjects` (`name`, `abbreviation`) VALUES ('Programování', 'PG');
 INSERT INTO `Subjects` (`name`, `abbreviation`) VALUES ('Informatika', 'IN');
@@ -16,8 +18,10 @@ INSERT INTO `Teachers` (`name`, `surname`, `prefix`, `suffix`) VALUES ('Tereza',
 INSERT INTO `Teachers` (`name`, `surname`, `prefix`, `suffix`) VALUES ('Zdeňka', 'Sobolová','','');
 INSERT INTO `Teachers` (`name`, `surname`, `prefix`, `suffix`) VALUES ('Ladislav', 'Štěpánek','Ing.','MBA');
 -- třídy
-INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '3', 'D', 4);
-INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '4', 'E', 1);
+INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '1', 'A', 1);
+INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '2', 'B', 2);
+INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '3', 'C', 3);
+INSERT INTO `Classes` (`year`, `grade`, `label`, `class_teacher_id`) VALUES (2023, '4', 'D', 4);
 -- studenti
 INSERT INTO `Students` (`name`, `surname`, `class_id`) VALUES ('Jan', 'Rozek', 1);
 INSERT INTO `Students` (`name`, `surname`, `class_id`) VALUES ('Jan', 'Horák', 1);
@@ -25,10 +29,18 @@ INSERT INTO `Students` (`name`, `surname`, `class_id`) VALUES ('Jan', 'Kolář',
 INSERT INTO `Students` (`name`, `surname`, `class_id`) VALUES ('Jan', 'Sirůček', 1);
 -- příznaky učeben
 INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (1, 1);
+
 INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (1, 2);
+INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (2, 2);
+
 INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (1, 3);
 INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (2, 3);
+INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (3, 3);
+
+INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (1, 4);
 INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (2, 4);
+INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (3, 4);
+INSERT INTO `ClassroomsTraits` (`trait_id`, `classroom_id`) VALUES (4, 4);
 -- příznaky předmětu
 INSERT INTO `SubjectsTraits` (`trait_id`, `subject_id`) VALUES (1, 1);
 INSERT INTO `SubjectsTraits` (`trait_id`, `subject_id`) VALUES (2, 2);
