f6836aef48058bf1bd7cca4d5805267fbccf4e32
pvorisek25 <pvorisek25@protonmail.com>
2023-02-15 19:43:27 +0100

    Přidány třídy DatabaseEntityProperty
    a DatabaseEntityPropertyType

---
+++ src/Model/DatabaseEntityProperty.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spse\NahradniHodnoceni\Model;
+
+class DatabaseEntityProperty {
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $displayName;
+
+    /**
+     * @var DatabaseEntityPropertyType
+     */
+    public $type;
+
+    /**
+     * @var bool
+     */
+    public $isSelect;
+
+    /**
+     * @var bool
+     */
+    public $isNullable;
+
+    /**
+     * @var mixed
+     */
+    public $defaultValue;
+
+    public function __construct($name, $displayName, $type, $isSelect, $isNullable, $defaultValue) {
+        $this->name = $name;
+        $this->displayName = $displayName;
+        $this->isSelect = $isSelect;
+        $this->isNullable = $isNullable;
+        $this->defaultValue = $defaultValue;
+    }
+
+    public function serialize($value): mixed {
+        if($this->type == DatabaseEntityPropertyType::DateTime) {
+            return $value->format("Y-m-d H:i:s");
+        } else {
+            return $value;
+        }
+    }
+
+    public function deserialize($value): mixed {
+        if($this->type == DatabaseEntityPropertyType::DateTime) {
+            return new \DateTime($value);
+        } else {
+            return $value;
+        }
+    } 
+}
---
+++ src/Model/DatabaseEntityPropertyType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spse\NahradniHodnoceni\Model;
+
+enum DatabaseEntityPropertyType {
+    case String;
+    case Integer;
+    case DateTime;
+}
